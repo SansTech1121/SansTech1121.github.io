90ca34d6a0fb81eb38114db5323bb5a1448928cc
SansTech1121 <139858994+SansTech1121@users.noreply.github.com>
2023-10-26 05:44:30 +0700

    xxxxx

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,4 @@
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
-
 export default defineNuxtConfig({
   ssr:false,
   //...
--- plugins/index.js
+++
@@ -1,4 +0,0 @@
-
-import * as syncfusion from '@syncfusion/ej2-base';
-
-syncfusion.License.registerLicense('ORg4AjUWIQA/Gnt2VlhhQlVFfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hSn9TdEBjXH1fcnZWT2VV');
