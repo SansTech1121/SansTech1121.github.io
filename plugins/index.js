c135a0bc11c2ec2e2136a41a10efa51338183871
SansTech1121 <139858994+SansTech1121@users.noreply.github.com>
2023-10-25 18:29:30 +0700

    sss

--- plugins/index.js
+++ plugins/index.js
@@ -2,6 +2,6 @@ import Vue from 'vue'
 import { registerLicense } from '@syncfusion/ej2-base';
 
 // Registering Syncfusion license key.
-registerLicense('ORg4AjUWIQA/Gnt2VlhhQlVFfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hSn9TdEBjXH1fcnZWT2VV');
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NHaF5cWWFCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdgWH9cdXRSRGldUE1yXEI=');
 // Component: <ClientOnly>
 Vue.component(ClientOnly.name, ClientOnly)
