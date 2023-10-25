4baa2c3c0b1a3c0b2e42da4297efd50271000347
SansTech1121 <139858994+SansTech1121@users.noreply.github.com>
2023-10-26 01:40:20 +0700

    ssss

--- pages/main.ts
+++ pages/main.ts
@@ -1,7 +1,7 @@
 /* Set up using Vue 3 */
 import { createApp } from 'vue'
-import App from '../layouts/Sidebar.vue'
-
+import App from './index.vue'
+import { registerLicense } from '@syncfusion/ej2-base';
 /* import the fontawesome core */
 import { library } from '@fortawesome/fontawesome-svg-core'
 
@@ -13,7 +13,7 @@ import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
 
 /* add icons to the library */
 library.add(faUserSecret)
-
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NHaF5cWWJCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdgWH9cdXVRRWlYUkx/XkU=');
 createApp(App)
 .component('font-awesome-icon', FontAwesomeIcon)
 .mount('#app')
